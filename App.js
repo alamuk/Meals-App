c58cfe376dc2b7bb07b37e7b6a47ffa849d9624d
ALAM <alam.2008@live.co.uk>
2024-09-06 11:41:10 +0100

    back to App

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigation from './src/route/StackNavigation';
-import DrawerApp from './NavigationPractice/DrawerNavigationPractice/DrawerApp';
-import TabApp from './NavigationPractice/TabNavigationPractice/TabApp';
 
 export default function App() {
   return (
     <>
-      <TabApp />
-
-      {/*<DrawerApp />*/}
-      {/*<StatusBar style="light" />*/}
-      {/*<NavigationContainer>*/}
-      {/*  <StackNavigation />*/}
-      {/*</NavigationContainer>*/}
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <StackNavigation />
+      </NavigationContainer>
     </>
   );
 }
